eb738d0ab6ba1e0fb68b12aaa7b57a3a79b6a16d
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-16 20:07:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,7 @@ end
 def batch_badge_creator(array)
   arr = []
 
-array.each do |badge| arr.push
-  ("Hello, my name is #{badge}.")
+array.each do |badge| arr.push("Hello, my name is #{badge}.")
 end
 return arr
 end
